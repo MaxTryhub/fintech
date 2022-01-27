ad98ae8d0f82b0f65e82de4b5339741cf50c43db
MaxTryhub <90391984+MaxTryhub@users.noreply.github.com>
2022-01-27 22:21:31 +0200

    Add files via upload

---
+++ fintech/exmp 1 - 2/exmp1.js
@@ -0,0 +1,3 @@
+const numbSpace =(x) =>{
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+}
---
+++ fintech/exmp 1 - 2/exmp2.js
@@ -0,0 +1,11 @@
+
+const input = IMask(element, {
+  mask: Number, 
+  scale: 2,  
+  signed: false, 
+  thousandsSeparator: '', 
+  padFractionalZeros: false, 
+  normalizeZeros: true,  
+  radix: ',', 
+  mapToRadix: ['.'] ,
+});
